cc0544744fb43eb2133e88d4161556a9fae60825
ruslana <ruslana.prus@ukr.net>
2025-03-15 12:45:56 +0100

    multiple consumers

--- src/main/java/com/peach/rabbitmq/consumer/RabbitConsumer.java
+++ src/main/java/com/peach/rabbitmq/consumer/RabbitConsumer.java
@@ -5,12 +5,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class RabbitConsumer {
     private static final Logger log = LoggerFactory.getLogger(RabbitConsumer.class);
 
-    @RabbitListener(queues = "peach.fixrate")
-    public void consume(String message) {
-        log.info("Consuming :{}", message);
+    @RabbitListener(queues = "peach.fixrate", concurrency = "3-7")
+    public void consume(String message) throws InterruptedException {
+        log.info("Thread {} consuming :{}", Thread.currentThread().getName(), message);
+        TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextLong(2000));
     }
 }
