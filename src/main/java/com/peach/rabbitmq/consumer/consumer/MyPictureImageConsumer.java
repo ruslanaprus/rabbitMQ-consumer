62194ae6259717a0850c696f047e8dd2a5a63588
ruslana <ruslana.prus@ukr.net>
2025-03-19 11:05:44 +0100

    manual rejection

--- src/main/java/com/peach/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
+++ src/main/java/com/peach/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
@@ -3,11 +3,16 @@ package com.peach.rabbitmq.consumer.consumer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.peach.rabbitmq.consumer.entity.Picture;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class MyPictureImageConsumer {
@@ -20,15 +25,19 @@ public class MyPictureImageConsumer {
     }
 
     @RabbitListener(queues = "q.mypicture.image")
-    public void listen(String message){
+    public void listen(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) {
         try {
             var picture = objectMapper.readValue(message, Picture.class);
             if (picture.getSize() > 9000) {
-                throw new AmqpRejectAndDontRequeueException("Picture size too large: " + picture);
+                channel.basicAck(tag, false);
             }
-            log.info("On image: {}", picture);
-        } catch (JsonProcessingException | AmqpRejectAndDontRequeueException e) {
-            throw new RuntimeException(e);
+            log.info("Processing image: {}", picture);
+
+            channel.basicAck(tag, false);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
         }
     }
 }
